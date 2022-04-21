03a94f3d7724eebcbdf6ba2efa9f366d4eff1d47
Zackar <zahar_medovshikov@mail.ru>
2022-04-21 18:14:54 +0300

    правки

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,6 +1,6 @@
 document.querySelector(".header__btn-open").addEventListener("click", function() {
-  document.querySelector(".header__nav").classList.add("active");
+  document.querySelector(".header__nav-modified").classList.add("active");
 })
 document.querySelector(".header__toggle").addEventListener("click", function() {
-  document.querySelector(".header__nav").classList.remove("active");
+  document.querySelector(".header__nav-modified").classList.remove("active");
 })
